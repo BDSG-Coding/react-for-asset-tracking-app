9790c35086982979925d51e79d71a57b783f3712
semegoita <semere5ha@yahoo.com>
2018-07-31 15:07:17 -0700

    all components created

--- assets/src/App.js
+++ assets/src/App.js
@@ -1,19 +1,45 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import AssetCard from "./components/AssetCard";
+import Wrapper from "./components/Wrapper";
+import assets from "./assets.json";
+import Jumbotron from "./components/Jumbotron";
 import './App.css';
+import { FormBtn } from './components/Form/FormBtn';
+import { Col, Container, Rows} from "./components/Grid";
+import {Form} from "./components/Form";
+
+
 
 class App extends Component {
+  state = {
+    assets
+  };
+  removeAsset = id => {
+    const assets = this.state.assets.filter(asset => asset.id !== id);
+    // Set this.state.friends equal to the new friends array
+    this.setState({ assets });
+
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Wrapper>
+        <Jumbotron> Asset Tracking app </Jumbotron>
+        <Grid/>
+        {/* {this.state.assets.map(asset => (
+          <AssetCard
+            removeAsset={this.removeAsset}
+            id={asset.id}
+            key={asset.id}
+            modelNumber={asset.modelNumber}
+            serialNumber={asset.serialNumber}
+            assetTag={asset.assetTag}
+            IPaddress={asset.IPaddress}
+            rackId={asset.rackId}
+          />
+        ))} */} 
+        <Form/>
+        <FormBtn/>
+      </Wrapper>
     );
   }
 }
---
+++ assets/src/components/AssetCard/AssetCard.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import './AssetCard.css';
+
+const AssetCard = props => {
+<div className="card">  
+    <div className= "img-container">
+        <img alt={props.modelNumber} src={props.image} />
+    </div>
+    <div className = "content">
+        <ul>
+        <li>
+          <strong>Model Number:</strong> {props.modelNumber}
+        </li>
+        <li>
+          <strong>Serial Number:</strong> {props.serialNumber}
+        </li>
+        <li>
+          <strong>IP Address:</strong> {props.IPaddress}
+        </li>
+        <li>
+          <strong>Asset Tag:</strong> {props.assetTag}
+        </li>
+        <li>
+          <strong>Rack Id:</strong> {props.rackId}
+        </li>
+        </ul>
+    </div>
+    <span onClick={() => props.removeAsset(props.id)} className="remove">
+      𝘅 
+    </span>
+</div>
+}
+ 
+export default AssetCard;
